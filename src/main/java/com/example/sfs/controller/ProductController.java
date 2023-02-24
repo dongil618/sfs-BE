66de2e65199a6986b4fbcf9dd2f4b7722dec9808
dongil618 <rlaehddlf618@gmail.com>
2023-02-25 01:33:30 +0900

    [SFS-28] feat : 자동 상품 등록 API 구현
    - GET /api/crawler/products 구현
    - POST /api/crawler/products 구현

--- src/main/java/com/example/sfs/controller/ProductController.java
+++ src/main/java/com/example/sfs/controller/ProductController.java
@@ -1,8 +1,32 @@
 package com.example.sfs.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.sfs.dto.product.GetCrawledProductsRequestDto;
+import com.example.sfs.dto.product.GetCrawledProductsResponseDto;
+import com.example.sfs.dto.product.PostCrawledProductsRequestDto;
+import com.example.sfs.service.ProductService;
+import lombok.RequiredArgsConstructor;
 
-@Controller
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/crawler/products")
+    public ResponseEntity<List<GetCrawledProductsResponseDto>> getCrawledProducts(@RequestBody GetCrawledProductsRequestDto crawledProductsRequestDto) {
+        List<GetCrawledProductsResponseDto> crawledProductsResponseDtos = productService.getCrawledProducts(crawledProductsRequestDto);
+        return new ResponseEntity(crawledProductsResponseDtos, HttpStatus.OK);
+    }
+
+    @PostMapping("/crawler/products")
+    public ResponseEntity<Void> saveCrawledProducts(@RequestBody List<PostCrawledProductsRequestDto> postCrawledProductsRequestDtos){
+        return new ResponseEntity<>(productService.saveCrawledProducts(postCrawledProductsRequestDtos), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/sfs/service/ProductService.java
+++ src/main/java/com/example/sfs/service/ProductService.java
@@ -1,4 +1,72 @@
 package com.example.sfs.service;
 
+import com.example.sfs.dto.crawler.ProductDto;
+import com.example.sfs.dto.product.GetCrawledProductsRequestDto;
+import com.example.sfs.dto.product.GetCrawledProductsResponseDto;
+import com.example.sfs.dto.product.PostCrawledProductsRequestDto;
+import com.example.sfs.model.Product;
+import com.example.sfs.repository.ProductRepository;
+import com.example.sfs.util.crawler.ProductCrawler;
+import com.example.sfs.util.crawler.ProductCrawlerStrategy;
+import com.example.sfs.util.crawler.ProductCrawlerStrategyFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductCrawlerStrategyFactory productCrawlerStrategyFactory;
+
+    /*
+    url을 통해 상품 가져오기
+     */
+    public List<GetCrawledProductsResponseDto> getCrawledProducts(GetCrawledProductsRequestDto crawledProductsRequestDto) {
+        String url = crawledProductsRequestDto.getUrl();
+        Integer pageNum = crawledProductsRequestDto.getPageNum();
+        ProductCrawlerStrategy siteType = crawledProductsRequestDto.getSiteType();
+
+        if(pageNum == null) {
+            throw new IllegalArgumentException("페이지 수를 설정해주세요.");
+        }
+        if(siteType == null) {
+            throw new IllegalArgumentException("웹 사이트 타입을 입력해주세요.");
+        }
+
+        ProductCrawler productCrawlerStrategy = productCrawlerStrategyFactory.findStrategy(siteType);
+        List<ProductDto> productDtos = productCrawlerStrategy.getProductThumbInfos(url, pageNum);
+
+        List<GetCrawledProductsResponseDto> crawledProductsResponseDtos = new ArrayList<>();
+        for(ProductDto productDto : productDtos) {
+            String productName = productDto.getName();
+            String detailPageUrl = productDto.getDetailPageUrl();
+            String thumbnailImageUrl = productDto.getThumbnailImageUrl();
+            crawledProductsResponseDtos.add(new GetCrawledProductsResponseDto(productName, detailPageUrl, thumbnailImageUrl));
+        }
+        return crawledProductsResponseDtos;
+    }
+
+    public Void saveCrawledProducts(List<PostCrawledProductsRequestDto> crawledProductsRequestDtos) {
+        ProductCrawlerStrategy siteType = crawledProductsRequestDtos.get(0).getSiteType();
+
+        if(crawledProductsRequestDtos == null) {
+            throw new IllegalArgumentException("요청이 잘못되었습니다.");
+        }
+        ProductCrawler productCrawlerStrategy = productCrawlerStrategyFactory.findStrategy(siteType);
+        List<ProductDto> productDtos = new ArrayList<>();
+        for(PostCrawledProductsRequestDto crawledProductsRequestDto : crawledProductsRequestDtos) {
+            ProductDto productDto = productCrawlerStrategy.getProductDetailInfo(crawledProductsRequestDto);
+            productDtos.add(productDto);
+        }
+        List<Product> products = new ArrayList<>();
+        for(ProductDto productDto : productDtos) {
+            products.add(new Product(productDto));
+        }
+        productRepository.saveAll(products);
+        return null;
+    }
 }
