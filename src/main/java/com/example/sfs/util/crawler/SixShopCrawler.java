24399bd9e85b2e7cb19e5a2a12c4fbf8501ca251
dongil618 <rlaehddlf618@gmail.com>
2023-02-25 01:31:54 +0900

    [SFS-28] feat : 자동 상품 등록 구현
    - JsoupTest.java로 jsoup 사용법 숙지
    - 상품 크롤러 전략 패턴 적용 및 cafe24 상품 크롤러 구현(아임웹, 식스샵은 추후 구현 예정)
    - cafe24 상품 크롤러 단위 테스트 코드 작성

---
+++ src/main/java/com/example/sfs/util/crawler/SixShopCrawler.java
@@ -0,0 +1,30 @@
+package com.example.sfs.util.crawler;
+
+import com.example.sfs.dto.crawler.ProductDto;
+import com.example.sfs.dto.product.PostCrawledProductsRequestDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SixShopCrawler implements ProductCrawler {
+    @Override
+    public List<ProductDto> getProductDetailInfos(String url) {
+        return null;
+    }
+
+    @Override
+    public ProductDto getProductDetailInfo(PostCrawledProductsRequestDto postCrawledProductsRequestDto) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> getProductThumbInfos(String url, Integer pageNum) {
+        return null;
+    }
+
+    @Override
+    public ProductCrawlerStrategy getProductCrawlerStrategy() {
+        return null;
+    }
+}
