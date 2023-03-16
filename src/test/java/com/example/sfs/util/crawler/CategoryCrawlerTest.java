7951c59f4692bd26927c7d1c410fbcac0b78fc03
dongil618 <rlaehddlf618@gmail.com>
2023-03-16 23:28:15 +0900

    [SFS-28] test : 카테고리 크롤러 테스트 코드 수정

--- src/test/java/com/example/sfs/util/crawler/CategoryCrawlerTest.java
+++ src/test/java/com/example/sfs/util/crawler/CategoryCrawlerTest.java
@@ -1,11 +1,16 @@
-package com.example.sfs.util;
+package com.example.sfs.util.crawler;
 
-import com.example.sfs.util.crawler.CategoryCrawler;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class CategoryCrawlerTest {
+
+    @Autowired
+    private CategoryCrawler categoryCrawler;
 
     @Test
     @DisplayName("네이버 쇼핑 검색 -> 상위 5개 카테고리 중 가장 많은 것 가져오기")
@@ -14,7 +19,7 @@ public class CategoryCrawlerTest {
         String productName = "여자 오버핏 후드티";
 
         //when
-        String productCategory = new CategoryCrawler().getProductCategory(productName);
+        String productCategory = categoryCrawler.getProductCategory(productName);
 
         //then
         Assertions.assertThat(productCategory).isEqualTo("패션의류/여성의류/티셔츠");
