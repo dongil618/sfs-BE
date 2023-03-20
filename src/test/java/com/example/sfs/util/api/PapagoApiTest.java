b6e799a81b82f71946f3ee350ecae54c94b9d996
dongil618 <rlaehddlf618@gmail.com>
2023-03-20 23:25:04 +0900

    [SFS-28] test : 파파고 API 테스트
    - 새로 파파고 API를 사용하기 때문에 테스트 코드 작성
    - 파파고 API를 의존성 주입하기 때문에 그에 따라 Cafe24CrawlerTest.java 변경

---
+++ src/test/java/com/example/sfs/util/api/PapagoApiTest.java
@@ -0,0 +1,35 @@
+package com.example.sfs.util.api;
+
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PapagoApiTest {
+
+    @Test
+    @DisplayName("파파고 API로 한글 영어로 변경")
+    public void changeEnToKorTest() {
+        // given
+        String enText = "Snap Button Two Tuck Pleats Trench Coat (Light Beige, Navy)";
+
+        // when
+        String koText = new PapagoApi().changeEnToKor(enText);
+
+        // then
+        Assertions.assertThat(koText).isEqualTo("스냅 버튼 투 턱 플리츠 트렌치 코트 (라이트 베이지, 네이비)");
+    }
+
+    @Test
+    @DisplayName("파파고 API로 언어 감지")
+    public void getLangCodeTest() {
+        // given
+        String enText = "Snap Button Two Tuck Pleats Trench Coat (Light Beige, Navy)";
+
+        // when
+        String langCode = new PapagoApi().getLangCode(enText);
+
+        // then
+        Assertions.assertThat(langCode).isEqualTo("en");
+    }
+}
